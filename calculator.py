2c300d1dbe4f511b4b04d7efe65ce205a8207903
sravankumar1238 <96710605+sravankumar1238@users.noreply.github.com>
2021-12-30 22:37:18 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,8 +5,8 @@ def sub(a, b):
     return a - b
 def mul(a, b):
     return a * b
-def div(a, b):
-    return a / b
+def add(a, b):
+    return a + b
 
 if __name__ == '__main__':
     print('run in calculator only')
