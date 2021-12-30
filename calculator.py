c75a374d3fd79116f1fa6aabd131ace728f869ba
sravankumar1238 <96710605+sravankumar1238@users.noreply.github.com>
2021-12-30 22:35:16 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+var = 999
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    print('run in calculator only')
