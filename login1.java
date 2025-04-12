6a3b5523547931364794d963822205918ef43191
sravan <kara.sravan16@gmail.com>
2025-04-12 11:08:46 +0000

    files modified

--- login1.java
+++ login1.java
@@ -3,7 +3,7 @@ public class login{
   2 public static void main() {
   3     int i;
   4 
-  5     for(i=15;i<25;i++){
+  5     for(i=30;i<25;i++){
   6         system.out.printIn("Number : " + i);
   7     }
   8 }
